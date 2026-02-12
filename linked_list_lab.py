c7561e07f00ee650861f0cf297edcfb855aed1dc
taff-kenrick <kenrick.taff@mail.bcu.ac.uk>
2026-02-12 22:17:33 +0000

    Create linked_list_lab.py

---
+++ linked_list_lab.py
@@ -0,0 +1,35 @@
+class Node:
+    def __init__(self, data):
+        self.data = data
+        self.next = None
+
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def insert_at_end(self, data):
+        new_node = Node(data)
+        if not self.head:
+            self.head = new_node
+            return
+        temp = self.head
+        while temp.next:
+            temp = temp.next
+        temp.next = new_node
+
+    def display(self):
+        temp = self.head
+        while temp:
+            print(temp.data, end=" -> ")
+            temp = temp.next
+        print("None")
+
+
+# build list
+ll = LinkedList()
+for v in [1, 2, 3, 4, 5]:
+    ll.insert_at_end(v)
+
+print("Linked list contents:")
+ll.display()
